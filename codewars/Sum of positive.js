4e8cd873228b4649c13bff9d8162dd5c015ccaa2
Irina <kharkova17@gmail.com>
2019-04-07 13:11:18 -0700

    create file-solutions for "Sum of positive"

---
+++ codewars/Sum of positive.js
@@ -0,0 +1,18 @@
+function positiveSum(arr) {
+    /*   let sum = 0;
+       for (let i = 0; i < arr.length; i++) {
+           if (arr[i] > 0) {
+               sum = sum + arr[i];
+           }
+       }
+       return sum;*/
+    let reducer = (sum, elm) => {
+        if (elm > 0) {
+            sum = sum + elm;
+        }
+        return sum;
+    };
+    return arr.reduce(reducer,0);
+}
+
+console.log(positiveSum([1, -2, -3, -4, 5]));
