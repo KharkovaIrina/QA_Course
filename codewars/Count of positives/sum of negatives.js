116dafd5ad1957f055f3f1b7696188369e61cc4d
Irina <kharkova17@gmail.com>
2019-04-07 13:52:37 -0700

    create file-solutions for "count of positive and sum of negative"

---
+++ codewars/Count of positives/sum of negatives.js
@@ -0,0 +1,17 @@
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0){
+        return [];
+    }
+    let count = 0;
+    let sum = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] > 0) {
+            count++;
+        } else {
+            sum = sum + input[i];
+        }
+    }
+    return [count,sum];
+}
+
+console.log(countPositivesSumNegatives([]));
