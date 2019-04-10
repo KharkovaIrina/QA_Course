df6a16bac911dd5b07bc4db43ebd82e7f988dc99
Irina <kharkova17@gmail.com>
2019-04-09 21:43:32 -0700

    create file-solution for task "Disemvowel Trolls"

--- codewars/Disemvowel Trolls.js
+++ codewars/Disemvowel Trolls.js
@@ -1,6 +1,8 @@
 function disemvowel(str) {
-    let regExp = /[^aeouiAEOUI]/g;
+  /*  let regExp = /[^aeouiAEOUI]/g;
     let arr = str.match(regExp);
-    return arr.join('');
+    return arr.join('');*/
+  let regExp = /[aeoui]/gi;
+  return str.replace(regExp,'');
 }
 console.log(disemvowel("This website is for losers LOL!"));
