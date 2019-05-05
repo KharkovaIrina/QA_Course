a56a689139bbcba70f36e3107a1e6f10679a767f
Irina <kharkova17@gmail.com>
2019-05-05 00:11:24 -0700

    create file 'INTERVIEW_1'

---
+++ codewars/INTERVIEW_1.js
@@ -0,0 +1,11 @@
+function myfunc(arr) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(!res.includes(arr[i])){
+            res.push(arr[i]);
+        }
+    }
+    return res;
+}
+
+console.log(myfunc([10, 2, 8, 7, 10, 8, 8, 5]));
