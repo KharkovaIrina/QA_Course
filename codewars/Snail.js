568a9e78b948baeedbd3d694b0566aa1060987b6
Irina <kharkova17@gmail.com>
2019-05-27 16:36:51 -0700

    create file-solution for kata 'Snail'

---
+++ codewars/Snail.js
@@ -0,0 +1,36 @@
+function shell(array, res) {
+
+    for (let elm of array.splice(0, 1)) {
+        for (let l of elm) {
+            res.push(l);
+        }
+    }
+
+    for (let i = 0; i < array.length - 1; i++) {
+        res.push(array[i].pop());
+
+    }
+
+    for (let elm of array.splice(array.length - 1)) {
+        for (let i = elm.length - 1; i >= 0; i--) {
+            res.push(elm[i]);
+        }
+    }
+
+    for (let i = array.length - 1; i >= 0; i--) {
+        res.push(array[i].shift());
+    }
+    if (array.length > 0) {
+        shell(array, res);
+    } else {
+        return res;
+    }
+}
+
+function snail(array) {
+    const result = [];
+    shell(array, result);
+    return result;
+}
+
+console.log(snail([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]));
